cd1625c2b34c796901910707cafa6c5e30814847
Yuri Zhdanov <varlogn@gmail.com>
2016-11-02 11:27:25 +0800

    fix django 1.9 compatibility

--- waffle/storage/__init__.py
+++ waffle/storage/__init__.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.utils.module_loading import import_by_path
+
+from django.utils.module_loading import import_string
 
 
 def default_storage(request):
@@ -7,4 +8,4 @@ def default_storage(request):
     Callable with the same interface as the storage classes.
     """
 
-    return import_by_path(settings.WAFFLE_STORAGE)(request)
+    return import_string(settings.WAFFLE_STORAGE)(request)
